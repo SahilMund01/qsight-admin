f66dc669a7feffc32b1411f76951df17c2aeea78
krishnaPatelRb2 <krishna.patel@techolution.com>
2024-09-11 15:40:57 +0530

    add sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Descope, useDescope } from '@descope/react-sdk';
+import { Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isActive, setActive] = useState(true);
+  const descopeSdk = useDescope();
+  const sessionToken = descopeSdk.getSessionToken();
+  const [loginResp, setLoginResp] = useState([]);
+
+  useEffect(() => {
+    console.log('resss', loginResp);
+    if (sessionToken) {
+      if (descopeSdk?.isJwtExpired(sessionToken)) {
+        setActive(false);
+      } else {
+        setActive(true);
+      }
+    } else {
+      setActive(false);
+    }
+  }, [sessionToken]);
+
+  const onLogout = async () => {
+    try {
+      const resp = await descopeSdk.logout();
+      console.log('Token', loginResp);
+    } catch (error) {
+      console.error('Logout failed', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {!isActive ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 'calc(100vh - 250px)', // Adjusted for viewport height
+          }}
+        >
+          <Descope
+            flowId="sign-in"
+            theme="light"
+            onSuccess={(e) => {
+              setLoginResp(e?.detail.user?.userTenants);
+              console.log(e?.detail);
+              console.log(e?.detail?.user?.email);
+            }}
+            onError={(err) => {
+              console.log('Error!', err);
+            }}
+          />
+        </div>
+      ) : (
+        <>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              marginBottom: '2rem',
+            }}
+          >
+            <Button onClick={onLogout} variant="contained" color="primary">
+              Logout
+            </Button>
+          </div>
+          <div
+            style={{
+              fontSize: '20px',
+              height: 'calc(100vh - 300px)', // Adjusted for viewport height
+            }}
+          >
+            {/* <div>tenantName - {loginResp[0]?.tenantName}</div>
+            <div>tenantId - {loginResp[0]?.tenantId}</div> */}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                gap: '1rem',
+              }}
+            >
+              <div style={{ fontWeight: '500' }}>tenantName -</div>
+              <div>{loginResp[0]?.tenantName}</div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                gap: '55px',
+              }}
+            >
+              <div style={{ fontWeight: '500' }}>tenantId -</div>
+              <div>{loginResp[0]?.tenantId}</div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from '@descope/react-sdk';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthProvider projectId="P2lLBXpqv8fNRZ7hdcoM1sDNTfgm">
+      <App />
+  </AuthProvider>
 )
